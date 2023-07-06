54bf391561f62fbd2d7fe695b3f3a47d89c24c4f
Jack <jarbus@tutanota.com>
2023-07-06 10:29:59 -0400

    add: generations class

--- main.py
+++ main.py
@@ -8,14 +8,61 @@ from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from dataclasses import dataclass, asdict
 
+
+
 @dataclass
 class Individual:
     prompt: str
     image: str
     pid: int
     ppid: int
+    ppid2: int
     mutations: tuple = ()
 
+# Global list to store each generation
+# first element is the parent, second is the child
+class Generations:
+    def __init__(self):
+        self.g = defaultdict(lambda: [[],[]])
+        self.lock = asyncio.Lock()
+    def __getitem__(self, ident):
+        return self.g[ident]
+    def set_genesis(self, ident: int, genesis_ind: Individual):
+        self.g[ident] = [[],[], []]
+        self.g[ident][0].append(genesis_ind)
+
+    async def add_member(self, ident: int, gen: int, prompt: str, prompt2:str=None):
+        # try 3 times to generate a unique prompt
+        # otherwise, just include the duplicate
+        prompt = prompt.strip('"').strip()
+        if prompt2:
+            prompt2 = prompt2.strip('"').strip()
+        new_prompt = None
+        for i in range(1):
+            new_prompt = await make_new_prompt(prompt, prompt2)
+            new_prompt = new_prompt.strip('"').strip()
+            if new_prompt != prompt:
+                break
+            if i < 1:
+                print("============Generated the same prompt")
+            else:
+                print("============GIVING UP, KEEPING DUPE=======")
+        new_image = await send_to_sd(new_prompt)
+        pid = hash(new_prompt)
+        ppid = hash(prompt.strip('"'))
+        ppid2 = hash(prompt2.strip('"')) if prompt2 else None
+            
+        new_member = Individual(new_prompt, new_image, pid, ppid, ppid2, ["crossover"])
+        
+        async with self.lock:
+            self.g[ident][gen].append(new_member)
+ 
+n_prompts = 16
+n_parents = 4
+gens = Generations()
+in_progress = defaultdict(set)
+# keeps track of which child we've already returned
+child_idx = defaultdict(int)
 
 llama_urls = ["http://localhost:8001/", "http://localhost:8010/"]
 sd_urls = [f"http://localhost:800{i}/" for i in range(2,6)]
@@ -23,6 +70,8 @@ sd_urls = [f"http://localhost:800{i}/" for i in range(2,6)]
 llambalancer = LoadBalancer(llama_urls)
 sdbalancer = LoadBalancer(sd_urls)
 
+with open("/home/garbus/interactivediffusion/blind_promptmaker/prompts.txt", "r") as f:
+    sd_prompt_list = f.readlines()
 
 class CrossoverPromptIdent(BaseModel):
     p1: str
@@ -34,43 +83,6 @@ class PromptIdent(BaseModel):
 class Ident(BaseModel):
     id: int
 
-with open("/home/garbus/interactivediffusion/blind_promptmaker/prompts.txt", "r") as f:
-    sd_prompt_list = f.readlines()
-
-origins = [
-    "http://localhost",
-    "http://localhost:3000", # intereactive
-]
-n_prompts = 16
-n_parents = 4
-
-app = FastAPI()
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*", "Access-Control-Allow-Origin"],
-)
-
-
-# Global list to store each generation
-# first element is the parent, second is the child
-popgen = defaultdict(lambda: [[],[]])
-in_progress = defaultdict(set)
-poplock = asyncio.Lock()
-# keeps track of which child we've already returned
-child_idx = defaultdict(int)
-
-#async def send_to_llama(prompt):
-#    full_prompt = apply_random_crossover(prompt)
-#    data = {"prompt": full_prompt,
-#        "use_beam_search": False,
-#        "n": 1,
-#        "max_tokens": 64,
-#        "temperature": 0.8,
-#    }
-#    return await llambalancer.distribute_request(data, "LLAMA")
 
 def apply_random_crossover(prompt1, prompt2):
     return f"""Human: Caption 1: {prompt1}
@@ -123,91 +135,78 @@ async def make_new_prompt(prompt, prompt2=None):
 async def send_to_sd(prompt):
     return await sdbalancer.distribute_request({"prompt": prompt}, "SD", "")
 
+ 
+origins = [
+    "http://localhost",
+    "http://localhost:3000", # intereactive
+]
 
-async def add_member(ident: int, gen: int, prompt: str, prompt2:str=None):
-    global popgen
-    # try 3 times to generate a unique prompt
-    # otherwise, just include the duplicate
-    prompt = prompt.strip('"').strip()
-    if prompt2:
-        prompt2 = prompt2.strip('"').strip()
-    new_prompt = None
-    for i in range(1):
-        new_prompt = await make_new_prompt(prompt, prompt2)
-        new_prompt = new_prompt.strip('"').strip()
-        if new_prompt != prompt:
-            break
-        if i < 1:
-            print("============Generated the same prompt")
-        else:
-            print("============GIVING UP, KEEPING DUPE=======")
-    new_image = await send_to_sd(new_prompt)
-    pid = hash(new_prompt)
-    ppid = hash(prompt.strip('"'))
-    new_member = Individual(new_prompt, new_image, pid, ppid, ["crossover"])
-    
-    async with poplock:
-        popgen[ident][gen].append(new_member)
-
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*", "Access-Control-Allow-Origin"],
+)
+ 
 @app.post("/genesis")
 async def genesis(p: PromptIdent):
-    global popgen, child_idx
+    global gens, child_idx
     child_idx[p.id] = 0
-    genesis_ind = Individual(p.prompt, "", hash(p.prompt.strip('"')),0, ["genesis"])
-    popgen[p.id] = [[],[]]
-    popgen[p.id][0].append(genesis_ind)
+    genesis_ind = Individual(p.prompt, "", hash(p.prompt.strip('"')),0, 0, ["genesis"])
+    gens.set_genesis(p.id, genesis_ind)
 
-    popgen[p.id].append([])
     tasks = []
     for _ in range(16):
-        tasks.append(add_member(p.id, 1, p.prompt))
+        tasks.append(gens.add_member(p.id, 1, p.prompt))
     print(p.id, "submitted all tasks")
     await asyncio.gather(*tasks)
     return {"message": "Genesis Prompt submitted successfully"}
 
 @app.post("/crossover_prompts")
 async def crossover_prompts(p: CrossoverPromptIdent):
-    global popgen
-    append_gen = len(popgen[p.id]) - 1
+    global gens
+    append_gen = len(gens[p.id]) - 1
     tasks = []
     for _ in range(4):
-        tasks.append(add_member(p.id, append_gen, p.p1, p.p2)) # this is async
+        tasks.append(gens.add_member(p.id, append_gen, p.p1, p.p2)) # this is async
     await asyncio.gather(*tasks)
     return {"message": "Prompt submitted successfully"} 
 
 @app.post("/submit_prompt")
 async def submit_prompt(p: PromptIdent):
-    global popgen
-    append_gen = len(popgen[p.id]) - 1
+    global gens
+    append_gen = len(gens[p.id]) - 1
     tasks = []
     for _ in range(4):
-        tasks.append(add_member(p.id, append_gen, p.prompt)) # this is async
+        tasks.append(gens.add_member(p.id, append_gen, p.prompt)) # this is async
     await asyncio.gather(*tasks)
     return {"message": "Prompt submitted successfully"}
 
 @app.post("/increment_generation")
 async def increment_generation(ident: Ident):
-    global popgen, child_idx
+    global gens, child_idx
     # not enough parents
-    if len(popgen[ident.id][0]) == 0 or (len(popgen[ident.id]) > 2 and len(popgen[ident.id][-2]) < n_parents):
+    if len(gens[ident.id][0]) == 0 or (len(gens[ident.id]) > 2 and len(gens[ident.id][-2]) < n_parents):
         raise HTTPException(status_code=400, detail="No prompts available")
-    popgen[ident.id].append([])
+    gens[ident.id].append([])
     child_idx[ident.id] = 0
     return {"message": "Generation incremented successfully"}
 
 
 @app.get("/get_new_children")
 async def get_new_children(ident: int):
+    global gens, child_idx
 
-    async with poplock:
-        global popgen, child_idx
-        print(ident, child_idx[ident], [len(pg) for pg in popgen[ident]])
+    async with gens.lock:
+        print(ident, child_idx[ident], [len(pg) for pg in gens[ident]])
 
-        if len(popgen[ident][-2]) <= child_idx[ident]:
+        if len(gens[ident][-2]) <= child_idx[ident]:
             raise HTTPException(status_code=204, detail="No new children available")
 
-        new_children = popgen[ident][-2][child_idx[ident]:]
+        new_children = gens[ident][-2][child_idx[ident]:]
         # convert from dataclass to dictionary
         new_children = [asdict(child) for child in new_children]
-        child_idx[ident] = len(popgen[ident][-2])
+        child_idx[ident] = len(gens[ident][-2])
     return {"children": new_children}
