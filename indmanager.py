f25bd33b890d31596fe4290193a8806be09e6984
Jack <jarbus@tutanota.com>
2023-07-13 10:35:16 -0400

    chg: Integer ids to string ids since js cant handle ints

--- indmanager.py
+++ indmanager.py
@@ -1,5 +1,6 @@
 import sys
 import json
+from typing import Union
 from .utils import clean
 from random import randint
 from sqlalchemy import Column, Integer, String, create_engine
@@ -10,12 +11,12 @@ Base = declarative_base()
 
 class Individual(Base):
     __tablename__ = 'individuals'
-    uid = Column(Integer, primary_key=True)
-    pid = Column(Integer)
-    genesis_id = Column(Integer)
+    uid = Column(String, primary_key=True)
+    pid = Column(String)
+    genesis_id = Column(String)
     prompt = Column(String)
-    ppid = Column(Integer)
-    ppid2 = Column(Integer)
+    ppid = Column(String)
+    ppid2 = Column(String)
     image = Column(String)
     gen = Column(Integer)
     mutation_info = Column(String)
@@ -23,21 +24,23 @@ class Individual(Base):
     def to_dict(self):
         return {c.name: getattr(self, c.name) for c in self.__table__.columns}
 
-def make_individual(genesis_id: int, 
+def make_individual(genesis_id: Union[str, int], 
                     prompt: str, 
-                    ppid: int, 
-                    ppid2: int, 
+                    ppid: Union[str,int], 
+                    ppid2: Union[str, int], 
                     image: str, 
                     gen: int, 
                     mutation_info: dict):
+    # Javascript can't handle large numbers, so we use
+    # strings to represent all IDs
     uid = randint(0, sys.maxsize)
     pid = hash(clean(prompt))
-    return Individual(uid=uid,
-        pid=pid,
-        genesis_id=genesis_id,
+    return Individual(uid=str(uid),
+        pid=str(pid),
+        genesis_id=str(genesis_id),
         prompt=prompt,
-        ppid=ppid,
-        ppid2=ppid2,
+        ppid=str(ppid),
+        ppid2=str(ppid2),
         image=image,
         gen=gen,
         mutation_info=json.dumps(mutation_info))
@@ -52,19 +55,19 @@ class IndManager:
         self.Session = sessionmaker(bind=self.engine)
 
     def add_individual(self, individual):
-        session = self.Session()
-        session.add(individual)
-        session.commit()
+        with self.Session() as session:
+            session.add(individual)
+            session.commit()
 
     def get_individuals_by_pid(self, genesis_id, pid):
-        session = self.Session()
-        inds = session.query(Individual).filter(Individual.genesis_id == genesis_id, Individual.pid == pid).all()
-        assert len(inds) < 3 # no more than three dupes
-        return inds[0]
+        with self.Session() as session:
+            inds = session.query(Individual).filter(Individual.genesis_id == genesis_id, Individual.pid == pid).all()
+            assert len(inds) < 3 # no more than three dupes
+            return inds[0]
 
-    def get_individuals_by_gen(self, genesis_id, gen, pids):
-        session = self.Session()
-        return session.query(Individual).filter(Individual.genesis_id == genesis_id, Individual.gen == gen, Individual.pid.notin_(pids)).all()
+    def get_individuals_by_gen(self, genesis_id, gen, seen_pids):
+        with self.Session() as session:
+            return session.query(Individual).filter(Individual.genesis_id == genesis_id, Individual.gen == gen, Individual.pid.notin_(seen_pids)).all()
 
     def compute_lineage(self, genesis_id, pid):
         lineage = []
